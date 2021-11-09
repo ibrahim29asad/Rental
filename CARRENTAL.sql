33179301a57e0f72299617a74a17ae80a442d6fe
ibrahim29asad <72579268+ibrahim29asad@users.noreply.github.com>
2021-11-09 15:13:45 -0700

    Update CARRENTAL.sql

--- CARRENTAL.sql
+++ CARRENTAL.sql
@@ -1,6 +1,8 @@
+DROP DATABASE IF EXISTS CARRENTAL;
 CREATE DATABASE CARRENTAL; 
 USE CARRENTAL;
 
+DROP TABLE IF EXISTS CUSTOMER;
 CREATE TABLE CUSTOMER (
 	License_no		int not null,
 	Address	   	    varchar(255)  not null,
@@ -12,20 +14,23 @@ CREATE TABLE CUSTOMER (
 	primary key (License_no)
 );
 
-
-CREATE TABLE BOOKING_DETAIL (
-	Reservation_no			varchar(255) not null,
-	Date_End				varchar(25) not null,
-	Location_Dropoff		varchar(255),
-    Location_Pickup			varchar(255) not null,
-    Date_Start				varchar(25),
-    License_no				int not null,
-    Employee_ID				int not null,
-    License_Plate           varchar(255) not null,
-	primary key (Reservation_no),
-    FOREIGN KEY(License_no) REFERENCES CUSTOMER(License_no),
-    FOREIGN KEY(Employee_ID) REFERENCES EMPLOYEE(Employee_ID),
-    FOREIGN KEY(License_Plate) REFERENCES CAR(License_Plate)
+CREATE TABLE RENTAL_CENTER
+(
+Store_Id int, 
+Postal_Code varchar(255),
+Street_Number varchar(255),
+City varchar(255),
+PRIMARY KEY(Store_Id)
+);
+CREATE TABLE INSURANCE
+(
+Policy_no int,
+Provider_Name varchar(255),
+Coverage varchar(255),
+Price int,
+Store_Id int,
+PRIMARY KEY(Policy_no),
+FOREIGN KEY(Store_Id) REFERENCES RENTAL_CENTER(Store_Id)
 );
 
 CREATE TABLE EMPLOYEE (
@@ -40,28 +45,18 @@ CREATE TABLE EMPLOYEE (
     
 );
 
-
-CREATE TABLE Payment (
+CREATE TABLE PAYMENT (
 	Card_No			    int not null,
 	Form_Of_Payment		varchar(255) not null,
 	CVV			        int not null,
     Name_On_Card		varchar(255) not null,
     Billing_Address     varchar(255) not null,
     Store_Id            int,
+    License_no      int,
 	primary key (Card_No),
     FOREIGN KEY(License_no) REFERENCES CUSTOMER(License_no),
     FOREIGN KEY(Store_Id) REFERENCES RENTAL_CENTER(Store_Id)
     
-);
-
-CREATE TABLE MAINTENANCE
-(
-License_Plate varchar(255),
-Mileage int,
-Service_done varchar(255),
-Cost int,
-PRIMARY KEY(License_Plate),
-FOREIGN KEY(License_Plate) REFERENCES CAR(License_Plate)
 );
 
 CREATE TABLE CAR
@@ -80,28 +75,8 @@ PRIMARY KEY(License_Plate),
 FOREIGN KEY(Store_Id) REFERENCES RENTAL_CENTER(Store_Id)
 );
 
-CREATE TABLE INSURANCE
-(
-Policy_no int,
-Provider_Name varchar(255),
-Coverage varchar(255),
-Price int,
-Store_Id int,
-PRIMARY KEY(Policy_no),
-FOREIGN KEY(Store_Id) REFERENCES RENTAL_CENTER(Store_Id)
-);
 
-CREATE TABLE RENTAL_CENTER
-(
-Store_Id int, 
-Postal_Code varchar(255),
-Street_Number varchar(255),
-City varchar(255),
-PRIMARY KEY(Store_Id)
-);
-
-CREATE TABLE RENT
-(
+CREATE TABLE RENT(
 License_no int,
 License_Plate varchar(255),
 PRIMARY KEY(License_no, License_Plate),
@@ -109,10 +84,50 @@ FOREIGN KEY(License_no) REFERENCES CUSTOMER(License_no),
 FOREIGN KEY(License_Plate) REFERENCES CAR(License_Plate)
 );
 
-CREATE TABLE LOGIN
-(
+CREATE TABLE LOGIN(
 Employee_ID int,
 Passwords varchar(255),
 PRIMARY KEY(Employee_ID),
 FOREIGN KEY(Employee_ID) REFERENCES EMPLOYEE(Employee_ID)
 );
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE BOOKING_DETAIL (
+	Reservation_no			varchar(255) not null,
+	Date_End				varchar(25) not null,
+	Location_Dropoff		varchar(255),
+    Location_Pickup			varchar(255) not null,
+    Date_Start				varchar(25),
+    License_no				int not null,
+    Employee_ID				int not null,
+    License_Plate           varchar(255) not null,
+	primary key (Reservation_no),
+    FOREIGN KEY(License_no) REFERENCES CUSTOMER(License_no),
+    FOREIGN KEY(Employee_ID) REFERENCES EMPLOYEE(Employee_ID),
+    FOREIGN KEY(License_Plate) REFERENCES CAR(License_Plate)
+);
+
+
+
+
+DROP TABLE IF EXISTS MAINTENANCE;
+CREATE TABLE MAINTENANCE
+(
+License_Plate varchar(255),
+Mileage int,
+Service_done varchar(255),
+Cost int,
+PRIMARY KEY(License_Plate),
+FOREIGN KEY(License_Plate) REFERENCES CAR(License_Plate)
+);
+
+
+
