502c561344d76efed637dfba6c7c26b89d95d811
ibrahim29asad <72579268+ibrahim29asad@users.noreply.github.com>
2021-12-13 12:59:23 -0700

    Update Main.js

--- componentsUpdateMaintenance/Main.js
+++ componentsUpdateMaintenance/Main.js
@@ -1,9 +1,31 @@
 import React from "react"
 
-export default function Main() {
+ export default function Payment() {
     return (
-        <main>
-            <h1 className="main--title">Update Maintenance</h1>
-        </main>
-    )
-}
+         <main>
+             <h1 className="main--title">Employee: Car Maintenance</h1>
+             <h2 className="main--title">Payment:</h2>
+             <form className = "main--form">
+                 <label>
+                     License Plate:
+                     <input type="text" name="License Plate" />
+                 </label>
+                 <label>
+                     Mileage: 
+                     <input type="text" name="Mileage" />
+                 </label>
+                <label>
+                     Services Done: 
+                     <input type="text" name="Service Done" />
+                 </label>
+
+                <label>
+                     Cost: 
+                     <input type="text" name="Cost" />
+                 </label>
+
+                <input type="submit" value="Submit" />
+                 </form>
+         </main>
+     )
+ } 
